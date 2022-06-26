160456f0cf34206148145e477e4c3a194ae5607b
Volgena <99212912+Volgena@users.noreply.github.com>
2022-06-26 11:56:50 +0300

    Add files via upload

---
+++ Tribonschi1.py
@@ -0,0 +1,11 @@
+
+def tribonacci(param):
+    f1, f2, f3 = 0, 0, 1
+    for i in range(param):
+        f1, f2, f3 = f2, f3, f1 + f2 + f3
+        yield f1
+
+
+f = tribonacci(35)
+for i in range(35):
+    print(next(f))
